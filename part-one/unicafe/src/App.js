92a1ec5a00d279f0d392cc86c4a6622875411a81
Терентьев Егор <e.terentyev@ruskon.local>
2023-08-11 14:26:17 +0400

    complete 1.8

--- part-one/unicafe/src/App.js
+++ part-one/unicafe/src/App.js
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {(props.good-props.bad)/props.total}</p>
+      <p>positive {props.positive} %</p>
+    </div>
+  )
+}
+
 const handleGoodClick = (good, setGood, bad, neutral, setTotal, setPositive, setAverage) => {
   const updatedGood = good + 1
   setGood(updatedGood)
@@ -19,7 +33,6 @@ const handleNeutralClick = (good, bad, neutral, setTotal, setNeutral) => {
 
   const updatedTotal = good + bad + updatedNeutral
   setTotal(updatedTotal)
-
 }
 
 const handleBadClick = (good, bad, setBad, neutral, setTotal) => {
@@ -28,7 +41,6 @@ const handleBadClick = (good, bad, setBad, neutral, setTotal) => {
 
   const updatedTotal = good + updatedBad + neutral
   setTotal(updatedTotal)
-  
 }
 
 const App = () => {
@@ -46,13 +58,7 @@ const App = () => {
       <button onClick={() => handleGoodClick(good, setGood, bad, neutral, setTotal, setPositive, setAverage)}>good</button>
       <button onClick={() => handleNeutralClick(good, bad, neutral, setTotal, setNeutral)}>neutral</button>
       <button onClick={() => handleBadClick(good, bad, setBad, neutral, setTotal)}>bad</button>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {(good-bad)/total}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} positive={positive} />
     </div>
   )
 }
